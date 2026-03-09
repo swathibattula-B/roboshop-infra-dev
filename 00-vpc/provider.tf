319cd4bdc550ebb10b8fbf8d6f5781ced8ff7243
swathi <battulaswathi15407@gmail.com>
2026-03-09 13:34:37 +0530

    Initial commit

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
+    }
+  }
+
+  backend "s3" {
+    bucket  = "mylife-myrule-dev" # Replace with your unique bucket name
+    key     = "roboshop-dev-vpc"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
